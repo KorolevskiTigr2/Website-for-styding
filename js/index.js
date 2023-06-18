f66aa848c510620f31e5bddda98a95c3eb7917f3
KorolevskiTigr2 <mega.medved87gmail.com>
2023-06-18 14:20:45 +0300

    build: loacal storage(index.js)

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 const btnDarkMode = document.querySelector('.dark_mode_btn');
 
+if(localStorage.getItem('Darkmode') === 'dark') {
+    btnDarkMode.classList.add('dark_mode_btn--active');
+    document.body.classList.add('dark');
+}
+
 btnDarkMode.onclick = function() {
     btnDarkMode.classList.toggle('dark_mode_btn--active');
-    document.body.classList.toggle('dark');
+    const isDark = document.body.classList.toggle('dark');
+
+    if(isDark){
+        localStorage.setItem('Darkmode', "dark");
+    } else {
+        localStorage.setItem("DarkMode", "light");
+    }
+
 }
