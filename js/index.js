d59d61874f105c64603d5a75510c34de4be22a9b
KorolevskiTigr2 <mega.medved87gmail.com>
2023-06-18 14:28:39 +0300

    add: check browser settings(index.js)

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 const btnDarkMode = document.querySelector('.dark_mode_btn');
 
+// check browser settings
+if(window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    btnDarkMode.classList.add('dark_mode_btn--active');
+    document.body.classList.add('dark');
+}
+
+// check dark theme
 if(localStorage.getItem('Darkmode') === 'dark') {
     btnDarkMode.classList.add('dark_mode_btn--active');
     document.body.classList.add('dark');
 }
 
+// dark/light button
 btnDarkMode.onclick = function() {
     btnDarkMode.classList.toggle('dark_mode_btn--active');
     const isDark = document.body.classList.toggle('dark');
