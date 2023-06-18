90bf081762b9174e7fd5a69d91cb5fccfa3f8161
KorolevskiTigr2 <mega.medved87gmail.com>
2023-06-18 14:09:36 +0300

    add: dark-theme variables

--- js/index.js
+++ js/index.js
@@ -2,4 +2,5 @@ const btnDarkMode = document.querySelector('.dark_mode_btn');
 
 btnDarkMode.onclick = function() {
     btnDarkMode.classList.toggle('dark_mode_btn--active');
+    document.body.classList.toggle('dark');
 }
